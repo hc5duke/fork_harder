e521b16ad3f761526f79e2ae56928f48e7f1dd75
hc5duke <hc5duke@gmail.com>
2012-07-15 23:47:09 -0700

    init

---
+++ replace.js
@@ -0,0 +1,4 @@
+var h2s = document.getElementsByTagName('h2');
+for (var h = 0; h < h2s.length; h++) {
+  h2s[h].textContent = h2s[h].textContent.replace(/Forking Repository/, 'Hardcore Forking Action');
+}
